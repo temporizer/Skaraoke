2a0b0d955649ccb5ddded69488e71df0a6d3cc5a
Kenny Scott <kenny.scott@brandefined.com>
2016-08-26 17:41:50 +0000

    change dir

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<title>Skaraoke</title>
 	<link href="https://fonts.googleapis.com/css?family=Permanent+Marker|Material+Icons" rel="stylesheet">
-	<link rel="stylesheet" type="text/css" href="Skaraoke/dist/css/styles.min.css">
+	<link rel="stylesheet" type="text/css" href="dist/css/styles.min.css">
 </head>
 <body>
 	<header id="main-header">
@@ -16,6 +16,6 @@
 	    <p>Please enjoy your stay</p>
 	</div>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-	<script src="Skaraoke/dist/js/scripts.min.js"></script>
+	<script src="dist/js/scripts.min.js"></script>
 </body>
 </html>
