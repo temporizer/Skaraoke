f307ca2fa3c7ef091bf9b91a35b4e9197d0ff636
Kenny Scott <kenny.scott@brandefined.com>
2016-08-26 10:03:50 -0700

    working on ui

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Skaraoke</title>
+	<link href="https://fonts.googleapis.com/css?family=Permanent+Marker|Material+Icons" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="Skaraoke/dist/css/styles.min.css">
+</head>
+<body>
+	<header id="main-header">
+		<h2 id="logo">Skaraoke</h2>
+		<button id="nav-toggle"><i class="material-icons">&#xE5D2;</i></button>
+	</header>
+	
+	<div id="gangsta-wrappa">
+	    <p>Please enjoy your stay</p>
+	</div>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+	<script src="Skaraoke/dist/js/scripts.min.js"></script>
+</body>
+</html>
--- src/js/0.scripts.js
+++ src/js/0.scripts.js
@@ -1,1 +1,11 @@
 /* scripts */
+
+(function($){
+    $("#nav-toggle").on('click', function(e){
+        var icon = '';
+        
+        $(this).toggleClass('open');
+        icon = ($(this).hasClass('open')) ? '&#xE5CD;' : '&#xE5D2';
+        $(this).html('<i class="material-icons">' + icon + '</i>')
+    })
+})(jQuery); /* global jQuery */
